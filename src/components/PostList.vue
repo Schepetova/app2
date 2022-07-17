0f5a755a61958996adb7c4e79abbde4fa9174c0d
Tanya Schepetova <yaroslavbelogromov@gmail.com>
2022-07-17 14:04:22 +0300

    First Commit
    First Commit

---
+++ src/components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+<div class = "post">
+    <div>
+          <div><strong>Название:</strong> {{post.title}}</div>  
+          <div><strong>Описание:</strong> {{post.body}}</div>
+    
+</div>
+<post-item  
+v-for="post in posts"
+:post="post"
+/>
+       </div>
+
+
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true
+
+
+        }
+    }
+
+}
+</script>
+        }
+    }
+}
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
+</style>
